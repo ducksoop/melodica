960a3364e553b2bd78667da6cf723a3007302587
ducksoop <torresj1997@outlook.com>
2023-12-20 15:31:19 -0600

    feat: fixup source maps

--- src/renderer/src/pages/Page.styles.tsx
+++
@@ -1,5 +0,0 @@
-import styled from '@emotion/styled'
-
-const StyledPage = styled.section``
-
-export default StyledPage
--- src/renderer/src/pages/Page.tsx
+++
@@ -1,7 +0,0 @@
-import StyledPage from './Page.styles'
-
-const Page = (): JSX.Element => {
-  return <StyledPage></StyledPage>
-}
-
-export default Page
